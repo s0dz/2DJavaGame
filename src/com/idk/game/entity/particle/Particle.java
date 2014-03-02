d31bd6e0b2117b52f9247a82b00bb29d6cf2b4c0
s0dz <garrett.mctear@gmail.com>
2014-03-01 23:12:58 -0600

    Particle man
    Particle man, particle man
    Doing the things a particle can
    What's he like? It's not important
    Particle man
    
    Is he a dot, or is he a speck?
    When he's underwater does he get wet?
    Or does the water get him instead?
    Nobody knows, Particle man
    
    Triangle man, Triangle man
    Triangle man hates particle man
    They have a fight, Triangle wins
    Triangle man
    
    Universe man, Universe man
    Size of the entire universe man
    Usually kind to smaller man
    Universe man
    
    He's got a watch with a minute hand,
    Millenium hand and an eon hand
    When they meet it's a happy land
    Powerful man, universe man
    
    Person man, person man
    Hit on the head with a frying pan
    Lives his life in a garbage can
    Person man
    
    Is he depressed or is he a mess?
    Does he feel totally worthless?
    Who came up with person man?
    Degraded man, person man
    
    Triangle man, triangle man
    Triangle man hates person man
    They have a fight, triangle wins
    Triangle man

--- src/com/idk/game/entity/particle/Particle.java
+++ src/com/idk/game/entity/particle/Particle.java
@@ -1,6 +1,7 @@
 package com.idk.game.entity.particle;
 
 import com.idk.game.entity.Entity;
+import com.idk.game.graphics.Screen;
 import com.idk.game.graphics.Sprite;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,9 +28,20 @@ public class Particle extends Entity
     public Particle( int x, int y, int life, int amount )
     {
         this( x, y, life );
+        
         for( int i = 0; i < amount - 1; i++ )
         {
             particles.add( new Particle( x, y, life ) );
         }
+    }
+    
+    public void update()
+    {
+        
+    }
+    
+    public void render( Screen screen )
+    {
+        
     }
 }
