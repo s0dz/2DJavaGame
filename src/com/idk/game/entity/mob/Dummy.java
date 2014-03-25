6afbc4f6dd281fda49595ff915c8ef754ace17a1
Garrett McTear <garrett.mctear@gmail.com>
2014-03-25 00:15:51 -0600

    Animating Mobs
    Adding sprite loading for dummy spritesheet.

--- src/com/idk/game/entity/mob/Dummy.java
+++ src/com/idk/game/entity/mob/Dummy.java
@@ -1,16 +1,24 @@
 package com.idk.game.entity.mob;
 
+import com.idk.game.graphics.AnimatedSprite;
 import com.idk.game.graphics.Screen;
 import com.idk.game.graphics.Sprite;
+import com.idk.game.graphics.SpriteSheet;
 
 public class Dummy extends Mob
 {
+    private AnimatedSprite down = new AnimatedSprite( SpriteSheet.dummy_down, 32, 32, 3 );
+    private AnimatedSprite up = new AnimatedSprite( SpriteSheet.dummy_up, 32, 32, 3 );
+    private AnimatedSprite left = new AnimatedSprite( SpriteSheet.dummy_left, 32, 32, 3 );
+    private AnimatedSprite right = new AnimatedSprite( SpriteSheet.dummy_right, 32, 32, 3 );
+    
+    
     public Dummy( int x, int y )
     {
         this.x = x << 4;
         this.y = y << 4;
         
-        sprite = Sprite.player_down;
+        sprite = down;
     }
     
     public void update()
