77b0a16b6127f9297a1ae9c9306296f23b1fcb86
Garrett McTear <garrett.mctear@gmail.com>
2014-03-30 22:08:05 -0600

    Bug fix: Dummy Spritesheet directions
    Made sure the spritesheets were being loaded into the correct direciton
    variables.

--- src/com/idk/game/graphics/SpriteSheet.java
+++ src/com/idk/game/graphics/SpriteSheet.java
@@ -23,10 +23,10 @@ public class SpriteSheet
     public static SpriteSheet player_right = new SpriteSheet( player, 1, 0, 1, 3, 32 );
     
     public static SpriteSheet dummy = new SpriteSheet( "/textures/sheets/dummy.png", 128, 96 );
-    public static SpriteSheet dummy_down = new SpriteSheet( dummy, 2, 0, 1, 3, 32 );
-    public static SpriteSheet dummy_up = new SpriteSheet( dummy, 0, 0, 1, 3, 32 );
-    public static SpriteSheet dummy_left = new SpriteSheet( dummy, 3, 0, 1, 3, 32 );
-    public static SpriteSheet dummy_right = new SpriteSheet( dummy, 1, 0, 1, 3, 32 );
+    public static SpriteSheet dummy_down = new SpriteSheet( dummy, 0, 0, 1, 3, 32 );
+    public static SpriteSheet dummy_up = new SpriteSheet( dummy, 1, 0, 1, 3, 32 );
+    public static SpriteSheet dummy_left = new SpriteSheet( dummy, 2, 0, 1, 3, 32 );
+    public static SpriteSheet dummy_right = new SpriteSheet( dummy, 3, 0, 1, 3, 32 );
     
     private Sprite[] sprites;
     
